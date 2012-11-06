fab869ff5bf5c3d7c192cfcaa16240235ef4aad2
Dimitar Ivanov <biggie@abv.bg>
2012-11-06 21:32:52 +0200

    version 1.7
    added turkish to latin
    added portuguese to latin

--- jquery.friendurl.js
+++ jquery.friendurl.js
@@ -1,5 +1,5 @@
 /*
- * jQuery FriendURL plugin 1.6
+ * jQuery FriendURL plugin 1.7
  *
  * http://www.bulgaria-web-developers.com/projects/javascript/friendurl/
  *
@@ -18,7 +18,10 @@
 		"п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я",
 		"А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", 
 		"П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ь", "Ю", "Я",
-		"Ї", "ї", "Є", "є", "Ы", "ы", "Ё", "ё"
+		"Ї", "ї", "Є", "є", "Ы", "ы", "Ё", "ё",
+		"ı", "İ", "ğ", "Ğ", "ü", "Ü", "ş", "Ş", "ö", "Ö", "ç", "Ç",
+		"Á", "á", "Â", "â", "Ã", "ã", "À", "à", "Ç", "ç", "É", "é", "Ê", "ê", "Í", 
+		"í", "Ó", "ó", "Ô", "ô", "Õ", "õ", "Ú", "ú"
 	];
 
 	var latin = [
@@ -26,7 +29,10 @@
 		"p", "r", "s", "t", "u", "f", "h", "ts", "ch", "sh", "sht", "a", "y", "yu", "ya",
 		"A", "B", "B", "G", "D", "E", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", 
 		"P", "R", "S", "T", "U", "F", "H", "Ts", "Ch", "Sh", "Sht", "A", "Y", "Yu", "Ya",
-		"I", "i", "Ye", "ye", "I", "i", "Yo", "yo"
+		"I", "i", "Ye", "ye", "I", "i", "Yo", "yo",
+		"i", "I", "g", "G", "u", "U", "s", "S", "o", "O", "c", "C",
+		"A", "a", "A", "a", "A", "a", "A", "a", "C", "c", "E", "e", "E", "e", "I",
+		"i", "O", "o", "O", "o", "O", "o", "U", "u" 
 	];
 	
 	var string = '';
@@ -97,7 +103,13 @@
 			$(target).keyup(function () {
 				options = $.extend(self.defaults, options);
 
-				var url = $(this).val()
+				var url = $(this).val();
+				
+				if (options.transliterate) {
+    				url = convert(url);
+    			}
+
+				url = url
     				.toLowerCase() // change everything to lowercase
     				.replace(/^\s+|\s+$/g, "") // trim leading and trailing spaces		
     				.replace(/[_|\s]+/g, "-") // change all spaces and underscores to a hyphen
@@ -106,10 +118,6 @@
     				.replace(/^-+|-+$/g, "") // trim leading and trailing hyphens
     				.replace(/[-]+/g, options.divider)				
     			;
-    			
-    			if (options.transliterate) {
-    				url = convert(url);
-    			}
     			
     			var $el = $('#' + options.id);
 
@@ -128,7 +136,7 @@
 	};
 	
 	$.friendurl = new Friendurl();
-	$.friendurl.version = "1.6";
+	$.friendurl.version = "1.7";
 	
 	$.fn.friendurl = function (options) {	
 		return this.each(function () {
